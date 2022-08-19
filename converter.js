300827a5cdecb11d5414d29e7557c3b8b5ee6377
Abdul-Hakim007 <abdulhakimjoydebpur@gmail.com>
2022-08-19 13:01:22 +0600

    first-converter

---
+++ converter.js
@@ -0,0 +1,8 @@
+const celsius = prompt("Enter a celsius value: ");
+// ask the user to input value
+
+const fahrenheit = (celsius * 1.8) + 32;
+// converting theory
+
+
+console.log(`${celsius} is equal to ${fahrenheit} fahrenheit.`);
